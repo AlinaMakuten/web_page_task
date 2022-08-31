4dba791e76eb8d00ba42cb175750e241d824dea2
AlinaMakuten <100312976+AlinaMakuten@users.noreply.github.com>
2022-08-31 23:21:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Please enter your name");
+      return false;
+    }
+  }
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("product-m");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+
+  }
